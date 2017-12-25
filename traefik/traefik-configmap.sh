f5687fb5c63ea2d1cf24f4fe1fc526ccd181e5b2
johnnycao“TestCIgit config --global user.na.r.johnnycao“ <johnnycao@chinasystems.com>
2017-12-25 12:22:11 +0800

    11

--- traefik/traefik-configmap.sh
+++ traefik/traefik-configmap.sh
@@ -1,3 +1,2 @@
-#!/bin/sh
 kubectl delete configmap traefik-configmap -n kube-system  
 kubectl create configmap traefik-configmap --namespace=kube-system --from-file=./tls.crt --from-file=./tls.key --from-file=./traefik.toml
